7d2aaf14aed848d94a545223b24068dc5ea2440e
daniel-yocupicio <dag_98@hotmail.com>
2025-03-08 00:12:21 -0600

    delete skia library and optimize resources

--- src/screens/Number/BlurBackground.jsx
+++ src/screens/Number/BlurBackground.jsx
@@ -1,12 +1,24 @@
-import { BackdropBlur, Canvas, Fill, Image, useImage } from '@shopify/react-native-skia';
 import React from 'react';
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, Text } from 'react-native';
 
 const BlurBackground = () => {
-  const image = useImage(require('../../assets/images/singin.png'));
-
   return (
-    <Canvas style={styles.container}>
+    <Text>a</Text>
+  )
+}
+
+export default BlurBackground
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        position: 'absolute',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+});
+
+    /*<Canvas style={styles.container}>
         <Image
             image={image}
             x={0}
@@ -21,17 +33,4 @@ const BlurBackground = () => {
         >
         <Fill color="rgba(255, 255, 255, 0.6)" />
       </BackdropBlur>
-    </Canvas>
-  );
-};
-
-export default BlurBackground;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        position: 'absolute',
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-    },
-});
+    </Canvas>*/
